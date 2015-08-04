c6b1cabc727aadbd2b366bbc6ed7d5a3d9838138
Ole Rixmann <or@travelping.com>
2015-08-04 09:52:46 +0200

    make dependencies depend on minimal version instead of exact version

--- mix.exs
+++ mix.exs
@@ -17,8 +17,8 @@ defmodule EctoTtl.Mixfile do
   defp deps do
     [{:postgrex, ">= 0.0.0", optional: true},
      {:mariaex, ">= 0.0.0", optional: true},
-     {:ecto, "~> 0.13.0"},
-     {:ecto_it, "~> 0.1.0", optional: true},
-     {:ecto_migrate, "~> 0.4.0"}]
+     {:ecto, ">= 0.13.0"},
+     {:ecto_it, ">= 0.1.0", optional: true},
+     {:ecto_migrate, ">= 0.4.0"}]
   end
 end
