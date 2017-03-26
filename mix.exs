0ad336e7d26c616ea470ed1a295cee785804f82d
Mathias Gumz <mathias.gumz@travelping.com>
2017-03-26 16:22:46 +0200

    support elixir-1.4
    this commit addresses several cosmetic issues upon which elixir-1.4
    complaints about.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule EctoTtl.Mixfile do
      version: "0.0.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   def application do
